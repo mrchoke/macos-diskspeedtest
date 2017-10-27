d3133e8d1db0f936719b685aa33e630ebb26c44e
Chatchai Jantaraprim <cj@cjv6.net>
2017-10-27 07:10:58 +0700

    simplify display label part of program

--- disk-speed-test.sh
+++ disk-speed-test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo -e "\n\n"
-seq  -f "#" -s '' 30; echo -e "\n"
-echo -e "macOS Disk Speed Test\n"
-seq  -f "#" -s '' 30
-echo -e "\n\n"
+cat <<EOT
+
+##############################
+macOS Disk Speed Test
+##############################
+
+EOT
 
 echo -n "Write Speed: "
 dd if=/dev/zero bs=2048k of=tstfile count=1024 2>&1 | awk  -F'[^0-9]*' 'END{print $4 / 1048576 ,"MB/sec"}'
